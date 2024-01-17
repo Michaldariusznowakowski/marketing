7c3b4f47fd72eea36fe0c737a4e3371160b81f61
Michał Dariusz Nowakowski <Michaldariusznowakowski@protonmail.com>
2024-01-17 18:00:40 +0100

    fix users

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -33,7 +33,6 @@
 
                     <form action="{{ route('admin.users.delete', $user->id) }}" method="POST">
                         @csrf
-                        @method('DELETE')
                         <button type="submit" class="bg-red-500 text-white px-4 py-2 rounded-md">Delete User</button>
                     </form>
                 </div>
